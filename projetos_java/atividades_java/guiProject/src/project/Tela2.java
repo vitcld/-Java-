d12c3c0caa9474fb9f000ec4e937400d62d38000
Victor Claudino <86791443+vitcld@users.noreply.github.com>
2024-05-27 21:12:32 -0300

    projeto feito em aula

---
+++ projetos_java/atividades_java/guiProject/src/project/Tela2.java
@@ -0,0 +1,68 @@
+package project;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Tela2 {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Tela2 window = new Tela2();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Tela2() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(121, 254, 1));
+		panel.setBounds(40, 25, 349, 65);
+		frame.getContentPane().add(panel);
+		
+		JLabel lblNewLabel = new JLabel("FELIZ 2012!");
+		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 50));
+		panel.add(lblNewLabel);
+		
+		JButton btnLogout = new JButton("Signout");
+		btnLogout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		btnLogout.setBounds(170, 227, 89, 23);
+		frame.getContentPane().add(btnLogout);
+	}
+}
