430860e5929f1f72f11b45ec48b5c943aba21af7
Victor Claudino <86791443+vitcld@users.noreply.github.com>
2024-06-03 14:10:06 -0300

    Delete projetos_java/atividades_java/projeto_A1 directory

--- projetos_java/atividades_java/projeto_A1/Main.java
+++
@@ -1,9 +0,0 @@
-package aulastelas;
-    
-    public class Main{
-        public static void main(String[] args) throws Exception {
-            ConectaBanco dao = new ConectaBanco();
-            dao.readDataBase();
-        }
-   
-    }
